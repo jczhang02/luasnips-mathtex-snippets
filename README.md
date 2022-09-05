c3707ae04c3eaa3c05eb7eb67c40feffc9a4a29c
jczhang02 <jczhang@live.it>
2022-09-05 18:28:13 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,92 @@
+<h1 align="center">Luasnips MathTex Snippets :kissing_cat:</h1>
+
+This repo contains mathtex snippets for `markdown` and `latex` filetypes.
+
+Forked from [iurimateus](https://github.com/iurimateus/luasnip-latex-snippets.nvim), add `markdown` filetype support.
+
+## Installation
+
+```lua
+use {
+  "iurimateus/luasnip-latex-snippets.nvim",
+  requires = { "L3MON4D3/LuaSnip", "preservim/vim-markdown", "lervag/vimtex" },
+  config = function()
+    vim.g.vim_markdown_math = true
+    require'luasnip-latex-snippets'.setup()
+    -- or setup({ use_treesitter = true })
+  end,
+  ft = { "tex", "markdown" },
+}
+```
+
+**Notice: your luasnips config should set `enable_autosnippets = true`**
+
+## Customization
+
+An example file, located in `~/.config/nvim/luasnippets/tex.lua`:
+
+```lua
+--- self Latex snippets
+
+local snips, autosnips = {}, {}
+
+local tex = {}
+
+tex.in_mathzone = function()
+	return vim.fn["vimtex#syntax#in_mathzone"]() == 1
+end
+
+tex.in_comment = function()
+	return vim.fn["vimtex#syntax#in_comment"]() == 1
+end
+
+tex.in_text = function()
+	return not tex.in_mathzone() and not tex.in_comment()
+end
+
+tex.in_align = function()
+	return vim.fn["vimtex#env#is_inside"]("align")[1] ~= 0
+end
+
+snips = {
+	s(
+		{ trig = "it", name = "italic", dscr = "Insert italic text." },
+		{ t("\\textit{"), i(1), t("}") },
+		{ condition = tex.in_text, show_condition = tex.in_text }
+	),
+	s(
+		{ trig = "em", name = "emphasize", dscr = "Insert emphasize text." },
+		{ t("\\emph{"), i(1), t("}") },
+		{ condition = tex.in_text, show_condition = tex.in_text }
+	),
+}
+
+autosnips = {
+	s({
+		trig = "([a-zA-Z])bf",
+		name = "math bold",
+		wordTrig = false,
+		regTrig = true,
+	}, {
+		f(function(_, snip)
+			return "\\mathbf{" .. snip.captures[1] .. "}"
+		end, {}),
+	}, { condition = tex.in_mathzone }),
+	s(
+		{ trig = "bf", name = "bold", dscr = "Insert bold text." },
+		{ t("\\textbf{"), i(1), t("}") },
+		{ condition = tex.in_text, show_condition = tex.in_text }
+	),
+	s(
+		{ trig = "bf", name = "bold", dscr = "Insert bold text." },
+		{ t("\\mathbf{"), i(1), t("}") },
+		{ condition = tex.in_mathzone, show_condition = tex.in_text }
+	),
+}
+
+return snips, autosnips
+```
+
+## Acknowledgement
+
+[iurimateus/luasnip-latex-snippets](https://github.com/iurimateus/luasnip-latex-snippets.nvim)
---
+++ lua/luasnip-latex-snippets/init.lua
@@ -0,0 +1,130 @@
+local ls = require("luasnip")
+local conds = require("luasnip.extras.expand_conditions")
+
+local utils = require("luasnip-latex-snippets.util.utils")
+local pipe = utils.pipe
+local no_backslash = utils.no_backslash
+
+local M = {}
+
+local default_opts = {
+  use_treesitter = false,
+}
+
+M.setup = function(opts)
+  opts = vim.tbl_deep_extend("force", default_opts, opts or {})
+
+  local is_math = utils.with_opts(utils.is_math, opts.use_treesitter)
+  local not_math = utils.with_opts(utils.not_math, opts.use_treesitter)
+
+  ls.config.setup({ enable_autosnippets = true })
+
+  ls.add_snippets("tex", {
+    ls.parser.parse_snippet(
+      { trig = "pac", name = "Package" },
+      "\\usepackage[${1:options}]{${2:package}}$0"
+    ),
+
+    -- ls.parser.parse_snippet({ trig = "nn", name = "Tikz node" }, {
+    --   "$0",
+    --   -- "\\node[$5] (${1/[^0-9a-zA-Z]//g}${2}) ${3:at (${4:0,0}) }{$${1}$};",
+    --   "\\node[$5] (${1}${2}) ${3:at (${4:0,0}) }{$${1}$};",
+    -- }),
+  })
+
+  local snippets = {}
+
+  local math_i = require("luasnip-latex-snippets/math_i")
+  for _, snip in ipairs(math_i) do
+    snip.condition = pipe({ is_math })
+    snip.show_condition = is_math
+    snip.wordTrig = false
+    table.insert(snippets, snip)
+  end
+
+  local autosnippets = {}
+
+  for _, snip in ipairs(require("luasnip-latex-snippets/math_wRA_no_backslash")) do
+    snip.regTrig = true
+    snip.condition = pipe({ is_math, no_backslash })
+    table.insert(autosnippets, snip)
+  end
+
+  for _, snip in ipairs(require("luasnip-latex-snippets/math_rA_no_backslash")) do
+    snip.wordTrig = false
+    snip.regTrig = true
+    snip.condition = pipe({ is_math, no_backslash })
+    table.insert(autosnippets, snip)
+  end
+
+  local tex_normal = {}
+
+  for _, snip in ipairs(require("luasnip-latex-snippets/normal_wA")) do
+    snip.condition = pipe({ not_math })
+    table.insert(tex_normal, snip)
+  end
+
+  for _, snip in ipairs(require("luasnip-latex-snippets/math_wrA")) do
+    snip.regTrig = true
+    snip.condition = pipe({ is_math })
+    table.insert(autosnippets, snip)
+  end
+
+  for _, snip in ipairs(require("luasnip-latex-snippets/math_wA_no_backslash")) do
+    snip.condition = pipe({ is_math, no_backslash })
+    table.insert(autosnippets, snip)
+  end
+
+  for _, snip in ipairs(require("luasnip-latex-snippets/math_iA")) do
+    snip.wordTrig = false
+    snip.condition = pipe({ is_math })
+    table.insert(autosnippets, snip)
+  end
+
+  for _, snip in ipairs(require("luasnip-latex-snippets/math_iA_no_backslash")) do
+    snip.wordTrig = false
+    snip.condition = pipe({ is_math, no_backslash })
+    table.insert(autosnippets, snip)
+  end
+
+  for _, snip in ipairs(require("luasnip-latex-snippets/math_bwA")) do
+    snip.condition = pipe({ conds.line_begin, is_math })
+    table.insert(autosnippets, snip)
+  end
+
+  for _, snip in ipairs(require("luasnip-latex-snippets/bwA")) do
+    snip.condition = pipe({ conds.line_begin, not_math })
+    table.insert(autosnippets, snip)
+  end
+
+  ls.add_snippets("tex", snippets, { default_priority = 0 })
+
+  ls.add_snippets("tex", autosnippets, {
+    type = "autosnippets",
+    default_priority = 0,
+  })
+  ls.add_snippets("tex", tex_normal, {
+    type = "autosnippets",
+    default_priority = 0,
+  })
+
+  local markdown_normal = {}
+
+  for _, snip in ipairs(require("luasnip-latex-snippets/normal_wA_markdown")) do
+    snip.condition = pipe({ not_math })
+    table.insert(markdown_normal, snip)
+  end
+
+  ls.add_snippets("markdown", snippets, { default_priority = 0 })
+
+  ls.add_snippets("markdown", autosnippets, {
+    type = "autosnippets",
+    default_priority = 0,
+  })
+  ls.add_snippets("markdown", markdown_normal, {
+    type = "autosnippets",
+    default_priority = 0,
+  })
+end
+
+return M
