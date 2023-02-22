91be8f9afbae664ea059e327bcc0f2122eb8f833
jczhang02 <jczhang@live.it>
2023-02-22 11:14:16 +0800

    feat: add mathscr

--- lua/luasnip-latex-snippets/math_iA.lua
+++ lua/luasnip-latex-snippets/math_iA.lua
@@ -61,6 +61,7 @@ local math_iA = {
 	ls.parser.parse_snippet({ trig = "<!", name = "normal" }, "\\triangleleft"),
 	ls.parser.parse_snippet({ trig = "floor", name = "floor" }, "\\left\\lfloor $1 \\right\\rfloor$0"),
 	ls.parser.parse_snippet({ trig = "mcal", name = "mathcal" }, "\\mathcal{$1}$0"),
+	ls.parser.parse_snippet({ trig = "mscr", name = "mathscr" }, "\\mathscr{$1}$0"),
 	ls.parser.parse_snippet({ trig = "//", name = "Fraction" }, "\\frac{$1}{$2}$0"),
 	ls.parser.parse_snippet({ trig = "\\\\\\", name = "setminus" }, "\\setminus"),
 	with_priority(ls.parser.parse_snippet({ trig = "->", name = "to" }, "\\to"), 100),
